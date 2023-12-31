7f1dea66db87aeb1e0e6b1092d92d0be3aa33e47
NigelOmondi <nigelomondi62@gmail.com>
2023-12-31 14:32:40 +0300

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/ env python3
 """Index xrange"""
+from typing import Tuple
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
     Calculate the start and end index for a page and page size.
 
