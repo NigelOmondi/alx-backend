2a857f1fbb6d9917cea82b8f29beea66f8f66eed
NigelOmondi <nigelomondi62@gmail.com>
2023-12-31 12:40:47 +0300

    Simple helper function

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/ env pyhon3
+"""Index xrange"""
+
+
+def index_range(page, page_size):
+    """
+    Calculate the start and end index for a page and page size.
+
+    :param page: The page number (1-indexed)
+    :param page_size: The number of items per page
+    :return: A tuple containing the start index and end index
+    """
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
