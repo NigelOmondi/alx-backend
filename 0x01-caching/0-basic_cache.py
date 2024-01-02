166bf3bb83abf4941d21ce64f40d2a528a66e2c7
NigelOmondi <nigelomondi62@gmail.com>
2024-01-02 18:25:35 +0300

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" 0-basic_cache module.
+Implements a basic caching system.
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class that inherits from BaseCaching.
+    It's a basic caching system with no limit.
+    """
+
+    def put(self, key, item):
+        """
+        Add an item to the cache.
+        :param key: Key where the item will be stored.
+        :param item: Item to store in the cache.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieve an item from the cache by key.
+        :param key: Key that maps to the item in the cache.
+        :return: The item stored in the cache or None.
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
