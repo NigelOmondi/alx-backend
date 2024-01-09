df2b4ec60bab52d7c5d7af5690206491a2b1ccc3
NigelOmondi <nigelomondi62@gmail.com>
2024-01-09 23:36:53 +0300

    Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -8,7 +8,7 @@ from typing import Union, Dict
 
 
 class Config:
-    """Represents a Flask Babel configuration.
+    """Represent a Flask Babel configuration.
     """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
